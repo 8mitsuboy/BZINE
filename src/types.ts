37b2cb3c2b68b2dd05502db54ff8260e6d25b056
8mitsuboy <hachimitsu8897@icloud.com>
2026-02-23 23:25:10 +0900

    Postインターフェースを新規作成し、タグインデックスページを追加

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export interface Post {
+  url: string;
+  frontmatter: {
+    title: string;
+    pubDate: string;
+    description: string;
+    author: string;
+    image: {
+      url: string;
+      alt: string;
+    };
+    tags: string[];
+  };
+}
